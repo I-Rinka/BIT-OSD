0435c994c7548a1ef80c56c4c9e356a5b362d810
I_Rinka <I_Rinka@outlook.com>
2020-12-15 00:29:49 +0800

    Finished

--- Windows/ChildProcess.cpp
+++ Windows/ChildProcess.cpp
@@ -2,7 +2,7 @@
 #include <unistd.h>
 int main(int argc, char const *argv[])
 {
-    std::cout << "Hi,my name is çŽ‹æ¢“ä¸ž" << std::endl;
+    std::cout << "Hi,my name is Íõè÷Ø©" << std::endl;
     sleep(3);
     return 0;
 }

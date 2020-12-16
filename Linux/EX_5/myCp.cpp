e9b7b212e365283efb40d488eafca3da7ac7af4c
I_Rinka <I_Rinka@outlook.com>
2020-12-16 16:52:15 +0800

    Linux done

--- Linux/EX_5/myCp.cpp
+++ Linux/EX_5/myCp.cpp
@@ -14,12 +14,9 @@ int main(int argc, char const *argv[])
     {
         //argv 1是源文件，argv 2是目标路径
         MoveFile(argv[1], argv[2]);
-        //judge dir(目标路径)
-        //可能类型：dir，软链接，文件
     }
     else
     {
-        // MoveFile("/root/Dev/BIT-OSD/Linux/EX_3", "/root/Dev/BIT-OSD");
         cout << "Too little argument!" << endl;
     }
 
@@ -35,20 +32,21 @@ void MoveFile(const char *path, const char *dest_path)
     strcat(new_dest, "/");
     strcat(new_dest, file_name);
 
+    //目录操作
     if (S_ISDIR(a.st_mode))
     {
-        mkdir(new_dest, a.st_mode);
-        //目录操作
         //在目标新建一个地址
-        //递归调用这个函数
+        mkdir(new_dest, a.st_mode);
+        //对目录里面的文件递归调用MoveFile
         DIR *dir = opendir(path);
         char *new_path = (char *)calloc(MAX_INPUT * 4, sizeof(char));
         while (true)
         {
             dirent *d = readdir(dir);
-            //在dest的什么地方新建文件夹？
+            //d是游标，用来获取文件名
             if (d != 0)
             {
+                //..的意思是读到了上一个文件夹，我们不需要
                 if (strncmp(d->d_name, "..", 10) == 0)
                 {
                     break;
@@ -67,17 +65,17 @@ void MoveFile(const char *path, const char *dest_path)
         }
         free(new_path);
     }
+    //软链接操作 待测试
     else if (S_ISLNK(a.st_mode))
     {
-        //软链接 待测试
         char *linked_path = (char *)calloc(MAX_INPUT, sizeof(char));
         readlink(path, linked_path, MAX_INPUT);
         symlink(linked_path, new_dest);
         free(linked_path);
     }
+    //普通文件
     else if (S_ISREG(a.st_mode))
     {
-        //普通文件
         creat(new_dest, a.st_mode);
         int file_old = open(path, O_RDONLY);
         int file_new = open(new_dest, O_WRONLY);

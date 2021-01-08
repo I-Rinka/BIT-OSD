f66a715322a32de5f4fa35d552989fd9101312bb
梓丞 王 <I_Rinka@outlook.com>
2021-01-08 16:39:49 +0800

    fixed code page bug

--- Windows/EX_2/ChildProcess.cpp
+++ Windows/EX_2/ChildProcess.cpp
@@ -2,7 +2,7 @@
 #include <Windows.h>
 int main(int argc, char const *argv[])
 {
-    std::cout << "Hi,my name is ï¿½ï¿½ï¿½ï¿½Ø©" << std::endl;
+    std::cout << "Hi,my name is Íõè÷Ø©" << std::endl;
     Sleep(10000);
     return 0;
 }
